232a6dbd9b7c734c2023b372b06e07a048cca615
ADITYARAJ PATIL <119296947+aspatil0@users.noreply.github.com>
2025-04-16 00:32:11 +0530

    Update book.php

--- LAB-BOOKING USING PHP&MYSQL/book.php
+++ LAB-BOOKING USING PHP&MYSQL/book.php
@@ -2,7 +2,6 @@
 session_start();
 if (!isset($_SESSION['dept'])) header("Location: index.php");
 
-$conn = new mysqli("localhost", "root", "", "lab_booking");
 $duration = intval($_POST['duration']);
 $free_time = date("H:i:s", time() + $duration * 60);
 $conn->query("UPDATE lab_status SET booked=1, booked_by='{$_SESSION['dept']}', free_time='$free_time'");
