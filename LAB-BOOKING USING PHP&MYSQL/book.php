60972d32c4dcaf8ad4e3cdfba5dee9b5fa8ce20f
ADITYARAJ PATIL <119296947+aspatil0@users.noreply.github.com>
2025-04-18 01:56:05 +0530

    Update book.php

--- LAB-BOOKING USING PHP&MYSQL/book.php
+++ LAB-BOOKING USING PHP&MYSQL/book.php
@@ -7,4 +7,5 @@ $free_time = date("H:i:s", time() + $duration * 60);
 $conn->query("UPDATE lab_status SET booked=1, booked_by='{$_SESSION['dept']}', free_time='$free_time'");
 
 header("Location: dashboard.php");
+
 ?>
